29b7998a241bd9413fe1b5f727e358c3710a64fb
MKShoji <matheusshoji@outlook.com>
2022-08-09 11:56:44 -0300

    commit inicial 2

---
+++ resources/views/contato.blade.php
@@ -0,0 +1,8 @@
+@extends('template.default')
+@section('content')
+
+    <section>
+        <h1> Contato </h1>
+    </section>
+
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('template.default')
+@section('content')
+
+    <section>
+        <h1> Home </h1>
+
+        <img src="{{url('images/sual.gif')}}">
+
+    </section>
+
+@endsection
